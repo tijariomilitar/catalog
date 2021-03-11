f93c452298b52bd883524f09f3a59deff9eee27e
tijariomilitar <tijariomilitar@gmail.com>
2021-03-11 14:59:00 -0300

    removed prices from catalog

--- public/javascripts/scripts/product/view/catalog.js
+++ public/javascripts/scripts/product/view/catalog.js
@@ -4,32 +4,32 @@ Product.view.catalog.filter = async (products, pagination) => {
 	var html = "";
 	for(let i in products){
 		if(products[i].product_id){
-			html += "<div class='box b2 container ground pointer box-hover padding-10 margin-top-5 border' onclick='window.location.href=`/product/show/"+products[i].product_id+"`'>";
+			html += "<div class='box b4 container ground pointer box-hover padding-10 margin-top-5 border' onclick='window.location.href=`/product/show/"+products[i].product_id+"`'>";
 		} else {
-			html += "<div class='box b2 container ground pointer box-hover padding-10 margin-top-5 border' onclick='window.location.href=`/product/package/show/"+products[i].id+"`'>";
+			html += "<div class='box b4 container ground pointer box-hover padding-10 margin-top-5 border' onclick='window.location.href=`/product/package/show/"+products[i].id+"`'>";
 		};
 		if(products[i].image){
-			html += "<div class='mobile-box b12 center'><img class='size-40' src='"+products[i].image+"' alt=''/></div>";
+			html += "<div class='mobile-box b4 center'><img class='height-50' src='"+products[i].image+"' alt=''/></div>";
 		} else {
-			html += "<div class='mobile-box b12 center'><img class='size-40' src='/images/product/no-product.png' alt=''/></div>";
+			html += "<div class='mobile-box b4 center'><img class='height-50' src='/images/product/no-product.png' alt=''/></div>";
 		};
-		html += `<div class="mobile-box b4 avant-garde center em09 padding-10 bold" style="color:#323232;">`+products[i].name+` - `+products[i].color+`</div>`;
-		html += "<div class='mobile-box b6 center border-left container'>";
-			html += "<div class='mobile-box b1 em08 italic'>À vista</div>"
-			html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+products[i].price.toFixed(2)+"</div>"
-		html += "</div>";
-		html += "<div class='mobile-box b6 center border-left container'>";
-			html += "<div class='mobile-box b1 em08 italic'>30 dias</div>"
-			html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+(products[i].price * 1.06).toFixed(2)+"</div>"
-		html += "</div>";
-		html += "<div class='mobile-box b6 center border-left container'>";
-			html += "<div class='mobile-box b1 em08 italic'>60 dias</div>"
-			html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+(products[i].price * 1.09).toFixed(2)+"</div>"
-		html += "</div>";
-		html += "<div class='mobile-box b6 center border-left container'>";
-			html += "<div class='mobile-box b1 em08 italic'>90 dias</div>"
-			html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+(products[i].price * 1.12).toFixed(2)+"</div>"
-		html += "</div>";
+		html += `<div class="mobile-box b3-4 avant-garde center em09 padding-10 bold" style="color:#323232;">`+products[i].name+` - `+products[i].color+`</div>`;
+		// html += "<div class='mobile-box b6 center border-left container'>";
+		// 	html += "<div class='mobile-box b1 em08 italic'>À vista</div>"
+		// 	html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+products[i].price.toFixed(2)+"</div>"
+		// html += "</div>";
+		// html += "<div class='mobile-box b6 center border-left container'>";
+		// 	html += "<div class='mobile-box b1 em08 italic'>30 dias</div>"
+		// 	html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+(products[i].price * 1.06).toFixed(2)+"</div>"
+		// html += "</div>";
+		// html += "<div class='mobile-box b6 center border-left container'>";
+		// 	html += "<div class='mobile-box b1 em08 italic'>60 dias</div>"
+		// 	html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+(products[i].price * 1.09).toFixed(2)+"</div>"
+		// html += "</div>";
+		// html += "<div class='mobile-box b6 center border-left container'>";
+		// 	html += "<div class='mobile-box b1 em08 italic'>90 dias</div>"
+		// 	html += "<div class='mobile-box b1 em10 center italic bold' style='color:#467846;'>$"+(products[i].price * 1.12).toFixed(2)+"</div>"
+		// html += "</div>";
 		html += "</div>";
 	};
 	document.getElementById("product-catalog-filter-table").innerHTML = html;
@@ -48,7 +48,7 @@ Product.view.catalog.filter = async (products, pagination) => {
 			html += "<div class='box one'><img class='image-card' src='/images/product/no-product.png' alt=''/></div>";
 		};
 		html += `<div class="box one avant-garde center em10 padding-10 bold" style="color:#323232;">`+products[i].name+` - `+products[i].color+`</div>`;
-		html += "<div class='box one em15 center italic bold' style='color:#467846;'>$"+products[i].price.toFixed(2)+"</div>"
+		// html += "<div class='box one em15 center italic bold' style='color:#467846;'>$"+products[i].price.toFixed(2)+"</div>"
 		html += "</div>";
 		html += "</div>";
 	};
